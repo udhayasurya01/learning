118ccc34351fd99ce1b3dcf48b3b40a73a3d6f3b
Enoch PC 3 <Enoch PC 3@DESKTOP-5A1EB38>
2025-03-06 12:29:21 +0530

    delete

--- sampleproject/src/sampleproject/Student.java
+++ sampleproject/src/sampleproject/Student.java
@@ -1,30 +1,28 @@
-package sampleproject;
-
-public 
-
-	class Student { 
-	    int id;
-	    String name;
-
-	    public void printStudent()
-	    {
-	        System.out.println("Id:" + id);
-	        System.out.println("Name:" + name);
-	    }
-	}
-
-	class GFG {
-	    public static void main(String[] args)
-	    {
-	        Student obj = new Student();
-
-	        obj.id = 1;
-	        obj.name = "ABC";
-
-	        obj.printStudent();
-	    }
-	}
-
-
-
-	
+//package sampleproject;
+//
+//public class Student { 
+//	    int id;
+//	    String name;
+//
+//	    public void printStudent()
+//	    {
+//	        System.out.println("Id:" + id);
+//	        System.out.println("Name:" + name);
+//	    }
+//	}
+//
+//	class GFG {
+//	    public static void main(String[] args)
+//	    {
+//	        Student obj = new Student();
+//
+//	        obj.id = 1;
+//	        obj.name = "ABC";
+//
+//	        obj.printStudent();
+//	    }
+//	}
+//
+//
+//
+//	
